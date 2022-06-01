fa58561f5b32586139163a6c6d9f55421ab31596
orangemeduza <kgolyako@gmail.com>
2022-06-01 20:13:34 +0300

    first real test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FirstTest {
+
+    @Test
+    void openSite1(){
+        open("https://demoqa.com/text-box");
+    }
+}
